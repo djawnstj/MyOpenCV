d81cb5b577501b4cdbfeff6fde176519da1887d6
JUNSEO <djawnstj44@naver.com>
2022-06-21 17:37:34 +0900

    1차 기능구현중

--- app/src/main/java/com/example/opencv/DiffFragment.kt
+++ app/src/main/java/com/example/opencv/DiffFragment.kt
@@ -5,13 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.opencv.databinding.FragmentConvertImageBinding
+import com.example.opencv.databinding.FragmentDiffBinding
 
 class DiffFragment : Fragment() {
 
     companion object { private const val TAG = "DiffFragment" }
 
-    private val binding by lazy { FragmentConvertImageBinding.inflate(layoutInflater) }
+    private val binding by lazy { FragmentDiffBinding.inflate(layoutInflater) }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
 
